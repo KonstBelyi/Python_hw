51785487499cb89ea2f2d618d4b5cc2675c10968
KonstBelyi <konstbelyi@gmail.com>
2019-08-05 14:18:18 +0300

    first commit

---
+++ lesson_1/practice.py
@@ -0,0 +1,28 @@
+print("Hello World!")
+print('Hello \'World!\'')
+print("Hello \"World!\"")
+print('Hello \n"World"!')
+
+'''
+\a  - alert
+\n  - new line
+\b  - backspace
+\'  
+\"  
+\\  
+\t  - horizontal tab
+
+'''
+
+print("===================")
+print(45)
+print(34 + 5)
+print("===================")
+a = 12
+b = 23
+i = a + b
+# print("Result of sum " + str(a) + " and " + str(b) + ":", i)
+# print(i)
+print("Result of sum {A} and {B}: {I}".format(B=b, I=i, A=a))
+print("Result of sum {0} and {1}: {2}".format(a, b, i))
+print("Result of sum {} and {}: {}".format(a, b, i))
