d176dced74922a9b94ba716489ab082631b71224
KonstBelyi <konstbelyi@gmail.com>
2019-08-06 23:38:42 +0300

    first commit

---
+++ lesson_2/task_12.py
@@ -0,0 +1,23 @@
+"""
+
+Задание 12. Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере
+(пробелы важны!). Первая строка содержит следующее значение, а втора строка содержит предыдущее значение введёного
+числа
+
+    ```
+        Please enter an integer number: 1234
+        The next number for the number 1234 is 1235.
+        The previous number for the number 1234 is 1233.
+
+     or
+
+        Please enter an integer number: 0
+        The next number for the number 0 is 1.
+        The previous number for the number 0 is -1.
+
+"""
+
+n = int(input('Please enter an integer number: '))
+print('The next number for the number', n, 'is', n+1, '\b.')
+print('The previous number for the number', n, 'is', n-1, '\b.')
+
