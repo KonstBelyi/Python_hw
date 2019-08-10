e64c9800b94e68cbce963b1214c18bd1218e07dd
KonstBelyi <konstbelyi@gmail.com>
2019-08-11 01:52:18 +0300

    forst commit

--- lesson_3/oper_task_3.py
+++ lesson_3/oper_task_3.py
@@ -5,3 +5,8 @@
     - Выведите его первую цифру после десятичной точки.
 
 """
+x = float(input('Please, enter a number with two digits after the decimal point: '))
+y = x % int(x)      # берем дробную часть
+print('The fractional part of the number {} is: {}.'.format(x, y))
+z = int(y * 10)     # получаем первую цыфру после десятичной точки
+print('The first digit after the decimal point is: {}.'.format(z))
