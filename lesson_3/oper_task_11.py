7b3c2fad1ef096c8c528b71a7d5546d8321496ac
KonstBelyi <konstbelyi@gmail.com>
2019-08-11 01:54:50 +0300

    first commit

--- lesson_3/oper_task_11.py
+++ lesson_3/oper_task_11.py
@@ -5,11 +5,11 @@
 """
 
 xyz = int(input('Enter a three-digit number: '))
-x = xyz // 100
+x = xyz // 100          # получаю первую цифру
 print('First digit in number', xyz, 'is:', x, '\b.')
-y = (xyz // 10) % 10
+y = (xyz // 10) % 10    # получаю вторую цифру
 print('Second digit in number', xyz, 'is:', y, '\b.')
-z = xyz % 10
+z = xyz % 10            # получаю третью цифру
 print('Third digit in number', xyz, 'is:', z, '\b.')
-summ = x + y + z
+summ = x + y + z        # получаю сумму трех цифр
 print('The sum of all digits in number', xyz, 'is:', x, '+', y, '+', z, '=', summ, '\b.')
